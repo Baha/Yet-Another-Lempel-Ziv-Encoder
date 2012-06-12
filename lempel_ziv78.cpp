57462697d1c22150c62482f71afdbc6360e0a521
Adrian Palacios <ukonat@gmail.com>
2012-06-12 16:22:47 +0200

    Added files for the lz78 compressor. It compiles.

---
+++ lempel_ziv78.cpp
@@ -0,0 +1,7 @@
+#include <cstdio>
+#include "lempel_ziv78.h"
+
+void LempelZiv78Encoder::encode()
+{
+  printf("yehe\n");
+}
---
+++ lempel_ziv78.h
@@ -0,0 +1,12 @@
+#ifndef _LEMPEL_ZIV_78_H_
+#define _LEMPEL_ZIV_78_H_
+
+#include "lempel_ziv.h"
+
+class LempelZiv78Encoder : public LempelZivEncoder
+{
+public:
+  void encode();
+};
+
+#endif
